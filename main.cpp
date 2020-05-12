bf58595a90841b543f358cfb23e3922b19ce563e
deve023 <adevedia@fi.uba.ar>
2020-05-12 00:53:32 -0300

    main cheuqea ahora input de la linea de comando

--- main.cpp
+++ main.cpp
@@ -1,10 +1,85 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <cstring>
+
 #include "Complejo.h"
 
 using namespace std;
 
-int main()
+#define USO_CORRECTO_MSG "El uso correcto es: ./tp0 -i imag.pgm -o imagtranf.pgm -t [exp(z) / z]"
+
+int main(int argc, char *argv[])
 {
+	// se chequea si el numero de argumentos esta dentro del rango correspondiente
+	if(argc < 3 || argc > 7)
+	{
+		cout << USO_CORRECTO_MSG << endl;
+		return 1;
+	}
+
+	// strings que almacenan las instrucciones del usuario
+	string input("cin");
+	string output("cout");
+	string funcion("");
+
+	for(int i = 1; i < argc; i++)
+	{
+		if(!strcmp(argv[i], "-i"))
+		{
+			if(i == argc - 1)
+			{
+				cout << USO_CORRECTO_MSG << endl;
+				return 1;
+			}
+
+			input = string(argv[++i]);
+		}
+
+		else if(!strcmp(argv[i], "-o"))
+		{
+			if(i == argc - 1)
+			{
+				cout << USO_CORRECTO_MSG << endl;
+				return 1;
+			}
+
+			output = string(argv[++i]);
+		}
+
+		else if(!strcmp(argv[i], "-f"))
+		{
+			if(i == argc - 1)
+			{
+				cout << USO_CORRECTO_MSG << endl;
+				return 1;
+			}
+
+			funcion = string(argv[++i]);
+		}
+
+		else
+		{
+			cout << USO_CORRECTO_MSG << endl;
+			return 1;
+		}
+	}
+
+	// TODO: pasar funcion a minusculas
+
+	// se verifican las funciones requeridas por el usuario
+	if( funcion.compare( string("z") ) && funcion.compare( string("exp(z)") ) )
+	{
+		cout << "Las transformaciones habilitadas son z y exp(z)" << endl;
+		return 1;
+	}
+
+	if(!input.compare( string("-") ))
+		input = string("cin");
+
+	if(!output.compare( string("-") ))
+		output = string("cout");
+
+	cout << input << " " << output << " " << funcion << endl; // Linea para probar que se esten guardando bien los inputs. ESTO NO VA
 
 	return 0;
 }
