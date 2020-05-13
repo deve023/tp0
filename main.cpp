965ad7b79933df702081b75ad8fede3462e7c7d4
deve023 <adevedia@fi.uba.ar>
2020-05-13 13:01:29 -0300

    Agregue unas macros y la funcion leerEntrada para que quede mas limpio el main

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 #define USO_CORRECTO_MSG "El uso correcto es: ./tp0 -i imag.pgm -o imagtranf.pgm -t [exp(z) / z]"
 
+#define FLAG_INPUT "-i"
+#define FLAG_OUTPUT "-o"
+#define FLAG_FUNCION "-f"
+
+// Guarda en input, output y funcion, las entradas correspondientes de la linea de comando
+// devuelve false si no se respeto el formato
+bool leerEntrada(int argc, char *argv[], string &input, string &output, string &funcion);
+
 int main(int argc, char *argv[])
 {
 	// se chequea si el numero de argumentos esta dentro del rango correspondiente
@@ -22,48 +30,10 @@ int main(int argc, char *argv[])
 	string output = "cout";
 	string funcion = "";
 
-	for(int i = 1; i < argc; i++)
+	if(!leerEntrada(argc, argv, input, output, funcion))
 	{
-		string aux = argv[i]; // se pasa el argumento a string para poder aplicarle las funciones de la clase string
-
-		if(!aux.compare("-i"))
-		{
-			if(i == argc - 1)
-			{
-				cout << USO_CORRECTO_MSG << endl;
-				return 1;
-			}
-
-			input = argv[++i];
-		}
-
-		else if(!aux.compare("-o"))
-		{
-			if(i == argc - 1)
-			{
-				cout << USO_CORRECTO_MSG << endl;
-				return 1;
-			}
-
-			output = argv[++i];
-		}
-
-		else if(!aux.compare("-f"))
-		{
-			if(i == argc - 1)
-			{
-				cout << USO_CORRECTO_MSG << endl;
-				return 1;
-			}
-
-			funcion = argv[++i];
-		}
-
-		else
-		{
-			cout << USO_CORRECTO_MSG << endl;
-			return 1;
-		}
+		cout << USO_CORRECTO_MSG << endl;
+		return 1;
 	}
 
 	// se verifican las funciones requeridas por el usuario
@@ -82,4 +52,40 @@ int main(int argc, char *argv[])
 	cout << input << " " << output << " " << funcion << endl; // Linea para probar que se esten guardando bien los inputs. ESTO NO VA
 
 	return 0;
+}
+
+bool leerEntrada(int argc, char *argv[], string &input, string &output, string &funcion)
+{
+	for(int i = 1; i < argc; i++)
+	{
+		string aux = argv[i]; // se pasa el argumento a string para poder aplicarle las funciones de la clase string
+
+		if(!aux.compare(FLAG_INPUT))
+		{
+			if(i == argc - 1)
+				return false;
+
+			input = argv[++i];
+		}
+
+		else if(!aux.compare(FLAG_OUTPUT))
+		{
+			if(i == argc - 1)
+				return false;
+
+			output = argv[++i];
+		}
+
+		else if(!aux.compare(FLAG_FUNCION))
+		{
+			if(i == argc - 1)
+				return false;
+
+			funcion = argv[++i];
+		}
+
+		else
+			return false;
+	}
+	return true;
 }
