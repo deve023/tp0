96b2f4c174a0cb5ec49db446a1711d0367c17804
deve023 <adevedia@fi.uba.ar>
2020-05-17 21:28:02 -0300

    cmdline class

--- Makefile
+++ Makefile
@@ -12,11 +12,14 @@ Pixel.o: Pixel.cpp Pixel.h Complejo.h
 Imagen.o: Imagen.cpp Imagen.h Pixel.h Complejo.h
 	$(CXX) $(CXXFLAGS) -c Imagen.cpp
 
-main.o: main.cpp Imagen.h
+cmdline.o: cmdline.h
+	$(CXX) $(CXXFLAGS) -c cmdline.cpp
+
+main.o: main.cpp Imagen.h cmdline.h
 	$(CXX) $(CXXFLAGS) -c main.cpp
 
-tp0: main.o Pixel.o Imagen.o Complejo.o
+tp0: main.o Pixel.o Imagen.o Complejo.o cmdline.o
 	$(CXX) $(CXXFLAGS) $^ -o tp0
 
-rm:
+clean:
 	rm *.o
--- main.cpp
+++ main.cpp
@@ -1,91 +1,96 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 #include "Imagen.h"
+#include "cmdline.h"
 
 using namespace std;
 
-#define USO_CORRECTO_MSG "El uso correcto es: ./tp0 -i imag.pgm -o imagtranf.pgm -t [exp(z) / z]"
+#define USO_CORRECTO_MSG "El uso correcto es: tp0 -f funcion [-i file] [-o file]"
 
-#define FLAG_INPUT "-i"
-#define FLAG_OUTPUT "-o"
-#define FLAG_FUNCION "-f"
+static void opt_input(string const &);
+static void opt_output(string const &);
+static void opt_funcion(string const &);
+static void opt_help(string const &);
 
-// Guarda en input, output y funcion, las entradas correspondientes de la linea de comando
-// devuelve false si no se respeto el formato
-bool leerEntrada(int argc, char *argv[], string &input, string &output, string &funcion);
+static option_t options[] = {
+	{1, "i", "input", "-", opt_input, OPT_DEFAULT},
+	{1, "o", "output", "-", opt_output, OPT_DEFAULT},
+	{1, "f", "funcion", NULL, opt_funcion, OPT_MANDATORY},
+	{0, "h", "help", NULL, opt_help, OPT_DEFAULT},
+	{0, },
+};
+
+static string funcion;
+static istream *iss = 0;	// Input Stream (clase para manejo de los flujos de entrada)
+static ostream *oss = 0;	// Output Stream (clase para manejo de los flujos de salida)
+static fstream ifs; 		// Input File Stream (derivada de la clase ifstream que deriva de istream para el manejo de archivos)
+static fstream ofs;		// Output File Stream (derivada de la clase ofstream que deriva de ostream para el manejo de archivos)
+
 
 int main(int argc, char *argv[])
 {
-	// se chequea si el numero de argumentos esta dentro del rango correspondiente
-	if(argc < 3 || argc > 7)
-	{
-		cout << USO_CORRECTO_MSG << endl;
-		return 1;
-	}
-
-	// strings que almacenan las instrucciones del usuario
-	string input = "cin";
-	string output = "cout";
-	string funcion = "";
-
-	if(!leerEntrada(argc, argv, input, output, funcion))
-	{
-		cout << USO_CORRECTO_MSG << endl;
-		return 1;
-	}
-
-	// se verifican las funciones requeridas por el usuario
-	if( funcion.compare("z") && funcion.compare("exp(z)"))
-	{
-		cout << "Las transformaciones habilitadas son z y exp(z)" << endl;
-		return 1;
-	}
-
-	if(!input.compare("-"))
-		input = "cin";
-
-	if(!output.compare("-"))
-		output = "cout";
-
-	cout << input << " " << output << " " << funcion << endl; // Linea para probar que se esten guardando bien los inputs. ESTO NO VA
-
+	cmdline cmdl(options);	// Objeto con parametro tipo option_t (struct) declarado globalmente. Ver línea 51 main.cc
+	cmdl.parse(argc, argv); // Metodo de parseo de la clase cmdline
+	
 	return 0;
 }
 
-bool leerEntrada(int argc, char *argv[], string &input, string &output, string &funcion)
+static void
+opt_input(string const &arg)
 {
-	for(int i = 1; i < argc; i++)
+	// Si el nombre del archivos es "-", usaremos la entrada
+	// estándar. De lo contrario, abrimos un archivo en modo
+	// de lectura.
+	//
+	if (arg == "-")
+		iss = &cin;		// Establezco la entrada estandar cin como flujo de entrada
+	else
 	{
-		string aux = argv[i]; // se pasa el argumento a string para poder aplicarle las funciones de la clase string
-
-		if(!aux.compare(FLAG_INPUT))
-		{
-			if(i == argc - 1)
-				return false;
-
-			input = argv[++i];
-		}
-
-		else if(!aux.compare(FLAG_OUTPUT))
-		{
-			if(i == argc - 1)
-				return false;
-
-			output = argv[++i];
-		}
-
-		else if(!aux.compare(FLAG_FUNCION))
-		{
-			if(i == argc - 1)
-				return false;
-
-			funcion = argv[++i];
-		}
-
-		else
-			return false;
+		ifs.open(arg.c_str(), ios::in); // c_str(): Returns a pointer to an array that contains a null-terminated
+										// sequence of characters (i.e., a C-string) representing
+										// the current value of the string object.
+		iss = &ifs;
 	}
-	return true;
+
+	// Verificamos que el stream este OK.
+	//
+	if (!iss->good()) {
+		cerr << "No se puede abrir " << arg << "." << endl;
+		exit(1);
+	}
+}
+
+static void opt_output(string const &arg)
+{
+	// Si el nombre del archivos es "-", usaremos la salida
+	// estándar. De lo contrario, abrimos un archivo en modo
+	// de escritura.
+	//
+	if (arg == "-")
+		oss = &cout;	// Establezco la salida estandar cout como flujo de salida
+	else
+	{
+		ofs.open(arg.c_str(), ios::out);
+		oss = &ofs;
+	}
+
+	// Verificamos que el stream este OK.
+	//
+	if (!oss->good())
+	{
+		cerr << "No se puede abrir " << arg << "." << endl;
+		exit(1);		// EXIT: Terminación del programa en su totalidad
+	}
+}
+
+static void opt_funcion(string const &arg)
+{}
+
+static void opt_help(string const &arg)
+{
+	cout << USO_CORRECTO_MSG << endl;
+	exit(0);
 }
